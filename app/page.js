2f9a9e89ef49a6b66948f9c9d182c9550a9f7b99
Büsra Sahin <buesra.sahin97@web.de>
2024-04-08 23:28:54 +0200

    add type Color Mapping

--- app/page.js
+++ app/page.js
@@ -1,6 +1,26 @@
 "use client"
 
 import { useEffect, useState } from 'react';
+const typeToColorMapping = {
+  normal: "#A8A878",
+  fire: "#F08030",
+  fighting: "#C03028",
+  water: "#6890F0",
+  flying: "#A890F0",
+  grass: "#78C850",
+  poison: "#A040A0",
+  electric: "#F8D030",
+  ground: "#E0C068",
+  psychic: "#F85888",
+  rock: "#B8A038",
+  ice: "#98D8D8",
+  bug: "#A8B820",
+  dragon: "#7038F8",
+  ghost: "#705898",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  fairy: "#EE99AC",
+};
 
 export default function Home() {
   const [data, setData] = useState(null);
@@ -41,9 +61,7 @@ export default function Home() {
         <p key={index}>{pokemon.name}</p>
       ))} */}
       {pokemonDetails && pokemonDetails.map((pkm, index) => (
-        <div key={index}>
-          <p>{pkm.name}</p>    
-          <img src={pkm.sprites.front_default}></img>  
+              <span key={index} className='px-2 py-1 drop-shadow rounded-lg' style={{ backgroundColor: typeToColorMapping[type.type.name] }} >
         </div>
       ))}
     </main>
